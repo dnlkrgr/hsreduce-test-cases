7f82a051069e26f8e2dd8c9e38dc5e2a70b2081a
Daniel Krueger <danielkrueger@protonmail.com>
2020-12-04 16:00:30 +0100

    interesting.sh was a little bit weak

--- ticket18098/interesting.sh
+++ ticket18098/interesting.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 timeout 10s ghc/_build/stage1/bin/ghc -O2 -dcore-lint -fforce-recomp Bug.hs > output.txt 2> error.txt
-grep "*** Core Lint errors : in result of Simplifier ***" output.txt
-grep "The type of this binder doesn't match the type of its RHS:" output.txt
-grep "Compilation had errors" error.txt
+grep "*** Core Lint errors : in result of Simplifier ***" output.txt && grep "The type of this binder doesn't match the type of its RHS:" output.txt && grep "Compilation had errors" error.txt

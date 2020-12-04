03b7b18d8c1935fc219243f5a68d4018f3644193
Daniel Krueger <danielkrueger@protonmail.com>
2020-12-04 15:27:21 +0100

    #18098 didn't have a proper ghc set up

--- ticket18098/Bug_hsreduce.hs
+++ ticket18098/Bug_hsreduce.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE BangPatterns, FlexibleContexts, GADTs, MultiParamTypeClasses, RankNTypes, ScopedTypeVariables, TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts, GADTs, MultiParamTypeClasses, RankNTypes, TypeFamilies #-}
 module Bug (
         gcons
     ) where
@@ -6,41 +6,38 @@ import Data.Functor.Identity
 import GHC.ST
 import GHC.Types
 gcons :: GVector v a => a -> v a -> ()
-gcons x v = gunstream $ bcons x $ gstream v
+gcons x v = gunstream $ bcons x $ bfromVector v
 {-# INLINE gcons #-}
 data New v a = New (forall s. ST s ())
 data Chunk v a
-  = Chunk (forall m. GVector v a => Mutable v () a -> m ())
+  = Chunk Int (forall m.
+               (PrimMonad m, GVector v a) => Mutable v (PrimState m) a -> m ())
 data Step s a where Yield :: a -> s -> Step s a
-instance Functor (Step s) where
-  {-# INLINE fmap #-}
-  fmap f (Yield x _) = Yield (f x) undefined
 data Stream m a = forall s. Stream (s -> m (Step s a)) s
 type Bundle = MBundle Identity
-data MBundle m v a
-  = Bundle {_sElems :: (),
-            sChunks :: Stream m (Chunk v a),
-            _sVector :: (),
-            sSize :: ()}
+data MBundle m v a = Bundle {sChunks :: Stream m (Chunk v a)}
+class Monad m => PrimMonad m where
+  type PrimState m
+instance PrimMonad (ST s) where
+  type PrimState _ = ()
 type family Mutable (v :: Type -> Type) :: Type -> Type -> Type
 class GMVector v a where
   gmbasicLength :: v () a -> Int
-  gmbasicUnsafeSlice :: () -> v () a -> v () a
+  gmbasicUnsafeSlice :: () -> () -> v () a -> v () a
   gmbasicUnsafeNew :: m (v () a)
-  gmbasicUnsafeWrite :: v () a -> a -> m ()
+  gmbasicUnsafeWrite :: v (PrimState m) a -> a -> m ()
   gmbasicUnsafeGrow :: v () a -> () -> m (v () a)
 class GMVector (Mutable v) a => GVector v a where
-  gbasicUnsafeCopy :: () -> v a -> m ()
+  gbasicUnsafeCopy :: v a -> m ()
 sfoldlM m w (Stream step t)
-  = foldlM_loop SPEC w t
+  = foldlM_loop w t
   where
-      foldlM_loop !_ z s
+      foldlM_loop z s
         = do r <- step s
              case r of {
                Yield x s'
                  -> do z' <- m z x
-                       foldlM_loop SPEC z' s' }
-{-# INLINE [1] sfoldlM #-}
+                       foldlM_loop z' s' }
 Stream stepa _ `sappend` Stream stepb _
   = Stream step (Left undefined)
   where
@@ -55,57 +52,47 @@ ssingleton x
   = Stream (return . step) undefined
   where
       step = Yield x
-Bundle _ ta _ _ `bmappend` Bundle _ tb _ _
-  = Bundle undefined (sappend ta tb) undefined undefined
 bfromVector :: GVector v a => v a -> Bundle v a
 bfromVector = bmfromVector
-blift (Bundle _ (Stream vstep t) _ _)
-  = Bundle
-      undefined
-      (Stream (return . runIdentity . vstep) t)
-      undefined
-      undefined
+blift (Bundle (Stream vstep t))
+  = Bundle (Stream (return . runIdentity . vstep) t)
+{-# INLINE [1] blift #-}
 bmfromVector v
-  = n `seq`
-        Bundle undefined (Stream vstep undefined) undefined undefined
+  = Bundle (Stream vstep undefined)
   where
-      n = v
-      vstep True
+      vstep _
         = return
-            (Yield (Chunk (\ _ -> gbasicUnsafeCopy undefined v)) undefined)
+            (Yield (Chunk undefined (\ _ -> gbasicUnsafeCopy v)) undefined)
 bcons = bmcons
-bmcons x = bmappend (bmsingleton x)
-bmsingleton x = bmfromStream (ssingleton x) undefined
+bmcons x
+  = (\ (Bundle ta) (Bundle tb) -> Bundle (sappend ta tb))
+      (bmsingleton x)
+bmsingleton x = bmfromStream (ssingleton x)
 {-# INLINE [1] bmsingleton #-}
 bmfromStream (Stream step _)
-  = Bundle undefined (Stream step' undefined) undefined
+  = Bundle (Stream step' undefined)
   where
       step' _
         = do r <- step undefined
-             return $ fmap (\ x -> Chunk (\ v -> gmbasicUnsafeWrite v x)) r
-bmchunks = sChunks
-nrun (New p) = p
-nunstream s = New (gmvunstream s)
-gmunsafeNew _ = gmbasicUnsafeNew
-gmvunstream s = gmvmunstream (blift s)
-{-# INLINE [1] gmvunstream #-}
+             return
+               $ (\ (Yield x _)
+                    -> Yield (Chunk 1 (\ v -> gmbasicUnsafeWrite v x)) undefined)
+                   r
 gmvmunstream s = gmvmunstreamUnknown s
+{-# INLINE [1] gmvmunstream #-}
 gmvmunstreamUnknown s
-  = do v <- gmunsafeNew undefined
-       _ <- sfoldlM copyChunk (v, 0) (bmchunks s)
+  = do v <- gmbasicUnsafeNew
+       _ <- sfoldlM copyChunk (v, undefined) (sChunks s)
        undefined
   where
-      copyChunk (v, i) (Chunk f)
-        = do let j = i
+      copyChunk (v, _) (Chunk n f)
+        = do let j = n
              v' <- if gmbasicLength v < j then
-                       gmunsafeGrow v undefined
+                       gmbasicUnsafeGrow v undefined
                    else
                        undefined
-             f (gmbasicUnsafeSlice undefined v')
-             return (undefined, j)
+             f (gmbasicUnsafeSlice undefined undefined v')
+             return (undefined, undefined)
 {-# INLINE gmvmunstreamUnknown #-}
-gmunsafeGrow v = gmbasicUnsafeGrow v
-gnew m = runST (nrun m)
-gstream v = gstream' v
-gstream' v = bfromVector v
-gunstream s = gnew (nunstream s)
+gunstream s
+  = runST ((\ (New p) -> p) (New (gmvmunstream (blift s))))
---
+++ ticket18098/init.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+cd ghc.nix
+git submodule init
+git submodule update
+cd ..
+
+cd ghc
+git submodule init
+git submodule update
+
+nix-shell ../ghc.nix --run "./boot && configure_ghc && hadrian/build -j --flavour=quick"
